385c9bf6679b2be33ba011e957bdf5edfb2bd828
agoogleuser <armia506@gmail.com>
2022-05-12 00:27:08 +0200

    Fixed zero at the beginning and end of pulse

--- Functions_Part1/createSquareSignal.m
+++ Functions_Part1/createSquareSignal.m
@@ -3,8 +3,8 @@ function [outputSignal, t] = createSquareSignal(timeStart, timeOn, timeEnd, numO
   t = linspace(0, timeEnd, numOfSamples);
   [~,T_end]   = min(abs(t - timeOn));     %Return the nearest index for the given timeOn in the vector t
   [~,T_start] = min(abs(t - timeStart));
-  %T_end--;  %optimize it for the output signal
-  %T_start--;%Makes the signal begins with 1 instead of 0 if timeStart == 0;
+  %T_end = T_end - 1 ;  %optimize it for the output signal
+  T_start = T_start-1;%Makes the signal begins with 1 instead of 0 if timeStart == 0;
   outputSignal =[zeros(1,T_start) ones(1, T_end-T_start) zeros(1, numOfSamples - T_end)];
 
 end
