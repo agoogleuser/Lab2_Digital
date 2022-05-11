6f6cc052c8ce4b498def1f09a4df9551f7065771
agoogleuser <armia506@gmail.com>
2022-05-12 00:26:46 +0200

    Fixed channel issues

--- Functions_Part1/channel_A.m
+++ Functions_Part1/channel_A.m
@@ -1,9 +1,9 @@
-function [output_t, output_f] = channel_A(printCHannelInfo, BW, frequencyRange, varargin)
+function [output_t, output_f,filter] = channel_A(BW, frequencyRange, varargin)
 
   symbolVector_t = varargin{1}; %%one variable
   inputLength = length(varargin{1});
-  if (nargin >= 2) %%two or more variables
-    for i=2:(nargin-4)
+  if (nargin >= 1+3) %%two or more variables
+    for i=2:(nargin-3)
       %Check if the input size is the same for each vector input.
       if (length(varargin{i}) ~= inputLength)
         error("Invalid size for input vectors");
@@ -19,18 +19,6 @@ function [output_t, output_f] = channel_A(printCHannelInfo, BW, frequencyRange, 
   [~, F_start] = min(abs(frequencyRange+BW));   %nearest index of -BW
   Ns = length(frequencyRange);
   filter = [zeros(1,F_start) ones(1,F_end - F_start-1) zeros(1, Ns - F_end+1) ];
-
-  %Plotting Filter Response
-  if (printCHannelInfo == true)
-    figure;
-    plot(frequencyRange, filter);
-    title("Band Limited Channel Frequency Impulse Response");
-    grid on
-    xlim([-200e3 200e3])
-    ylim([0 1.5])
-    xlabel("frequency (f)");
-    ylabel("Amplitude (v)");
-  end
 
    %Affecting the input signals with the channel's response
   symbolVector_f = fftshift(fft(symbolVector_t));
