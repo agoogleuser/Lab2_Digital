fce7105e4a6dc51289fe60579b0a64ac587bcf70
agoogleuser <armia506@gmail.com>
2022-05-12 18:45:10 +0200

    Changed the frequency domain representation to make it more sharp.
    Added Pulse Shaping and decoding.

--- main2.m
+++ main2.m
@@ -1,39 +1,52 @@
-%%PART 1: ISI effect in Band-Limited Channel;
+%% PART 1: ISI effect in Band-Limited Channel;
 %This file is without plots.
 %Adding the folder path of the functions in order to run it.
 addpath(genpath("Functions_Part1"));
-%% Vars
-figurenum=1;          % To make numbering figures easter
-BW = 100e3;           % Channel Bandwidth
-T = 2 / BW;           % Pulse On time
-Ns= 10000;            % Number of samples
-timeSequence = 3*T;   % Total time Period which the simulation will calculate
-fs =Ns/timeSequence;  % Sampling Frequency
+%%Vars
+figurenum=1;                        % To make numbering figures easter
+BW = 100e3;                         % Channel Bandwidth
+T  = 2 / BW;                        % Pulse On time
+Ns = 1e6;                           % Number of samples
+Fs = 10e6;                          % Sampling Frequency
+Ts = 1/Fs;                          % Sampling Time
+
+t  = (0:Ns-1) * Ts;                 % Time Vector
+f  = (-0.5*Ns:0.5*Ns-1) * Fs/Ns;    % Frequency Vector
+
 %% par1_1
 %Creating Signal 1 and Signal 2 and converting them to frequency domain
-[signal1_t, t] = createSquareSignal(0,T,timeSequence, Ns);
-[signal2_t, ~] = createSquareSignal(T, 2*T, timeSequence, Ns);
-t=t*1e6; %converting time to microseconds.
+signal1_t = createSquareSignal(0,T,t);
+signal2_t = createSquareSignal(T, 2*T, t);
 signal1_f = fftshift(fft(signal1_t));
 signal2_f = fftshift(fft(signal2_t));
-f = (-length(signal1_f)/2:length(signal1_f)/2-1) * (fs/Ns);
 
 %Passing Signal 1 only into the channel.
-[output1_t, output1_f, filter] = channel_A(BW, f, signal1_t); % true means print Channel Frequency response
+[output1_t, output1_f, filter] = channel_A(0,BW, f, signal1_t); % true means print Channel Frequency response
 
 %Passing Signal 2 only into the channel.
-[output2_t, output2_f] = channel_A(BW, f, signal2_t);
+[output2_t, output2_f] = channel_A(0,BW, f, signal2_t);
 
 %%Passing Both signal 1 and signal 2 into the channel and getting the output.
-[output3_t, output3_f] = channel_A(BW, f, signal1_t, signal2_t);
+[output3_t, output3_f] = channel_A(0,BW, f, signal1_t, signal2_t);
 
-%%TODO: Plot the variables.
+%% TODO: Plot the variables.
 
 
-%% Digital Source and convert it to analog, 1000 sample
-signal1_t = createSquareSignal(0, T, T, Ns);
+%% Digital Source and convert it to analog
+signal1_t = createSquareSignal(0, T, t);
 signal2_t = zeros(1, Ns);
+message = [1 1 0 0 1 1 0 1 0 1];
 
-Tx = pulseShaping([0 1 1 0 1 0 0 1], signal2_t, signal1_t);
-Rx = ourDecoder(Tx, signal2_t, signal1_t)
+Tx_t = pulseShaping(message, signal2_t, signal1_t, T, t);
+Rx_t = channel_A(10, BW, f, Tx_t);
 
+figure;
+subplot(2,2,1)
+plot(t, Tx_t)
+xlim([0 T*length(message)])
+grid on;
+
+subplot(2,2,3)
+plot(t, Rx_t)
+xlim([0 T*length(message)])
+grid on;
